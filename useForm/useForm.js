30d966e79591a5c6e8dff2f5303292e6b9eeaf6a
Josue Perez <josueperezf@gmail.com>
2021-08-14 12:12:38 -0400

    modicar metodo reset
    si llamamos a reset sin pasarle parametros, colocara todos los inputs en vacio, pero si se le pasa parametros a reset, colocara dichos valores al form

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 
 // initialState es el nombre de los input que deseo que tenga el formulario
-export const useForm = (initialState= {}) => {
+export const useForm = (initialState = {}) => {
     const [values, setvalues] = useState(initialState);
 
-    const reset = ()=>{
-        setvalues(initialState);
+    // reset en los input su valor inicial, pero si se le envia una nueva data, entonces coloca esos valores nuevo a los value de los input
+    const reset = (newStateForm = initialState)=>{
+        setvalues(newStateForm);
     }
     const handleInputChange = ({target})=>{
         setvalues({
